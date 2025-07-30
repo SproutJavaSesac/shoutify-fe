1361bcb7c569540c10c1bcfd90de74047422bdb5
Sooamazing <honang515@gmail.com>
2025-07-30 17:59:06 +0900

    [#43]Feat(apis): 공통으로 사용할 Api hooks에서의 요청, 응답 형식

--- types/apis.ts
+++ types/apis.ts
@@ -11,3 +11,20 @@ export class ApiError extends Error {
     this.name = "ApiError";
   }
 }
+
+/**
+ * API 호출 hooks에서 사용하는 공통 응답 형식입니다.
+ */
+export interface ApiState<T> {
+  data: T | null;
+  loading: boolean;
+  error: Error | null;
+}
+
+/**
+ * get이 아닌 http method를 사용하는 API에서 성공, 실패 시 동작을 정의하는 옵션입니다.
+ */
+export type MutationOptions<R = any, E = any> = {
+  onSuccess?: (data?: R) => void;
+  onError?: (error: E) => void;
+};
