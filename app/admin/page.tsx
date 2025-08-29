23e9538e335728d17e8b12da6d91e895ba78eef7
Sooamazing <honang515@gmail.com>
2025-08-29 19:02:21 +0900

    [#63]Chore(presentation): 발표 위해 임시로 admin 페이지 권한 확인 해제

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,10 +1,9 @@
 import { AdminPanel } from "@/components/admin/admin-panel";
-import { AdminOnlyRoute } from "@/components/guards";
 
 export default function AdminPage() {
   return (
-    <AdminOnlyRoute>
-      <AdminPanel />
-    </AdminOnlyRoute>
+    // <AdminOnlyRoute>
+    <AdminPanel />
+    // </AdminOnlyRoute>
   );
 }
